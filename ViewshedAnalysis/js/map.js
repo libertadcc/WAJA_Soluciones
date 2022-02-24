38e1f65e094a67580ee6f7510b79b262bc3fb68f
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2022-02-24 20:00:31 +0100

    ADD viewshed exercise

--- ViewshedAnalysis/js/map.js
+++ ViewshedAnalysis/js/map.js
@@ -11,6 +11,10 @@ require([
     "esri/symbols/SimpleFillSymbol",
     "esri/Color",
 
+    "esri/tasks/Geoprocessor",
+    "esri/tasks/FeatureSet",
+    "esri/tasks/LinearUnit",
+
     "dojo/ready",
     "dojo/parser",
     "dojo/on",
@@ -18,6 +22,8 @@ require([
     function (Map, Draw, Graphic, graphicsUtils, 
         
         SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol, Color,
+
+        Geoprocessor, FeatureSet,LinearUnit,
         
         ready, parser, on, array) {
 
@@ -34,14 +40,13 @@ require([
             });
 
             // Step: Construct the Geoprocessor
+            var gp = new Geoprocessor('http://sampleserver6.arcgisonline.com/arcgis/rest/services/Elevation/ESRI_Elevation_World/GPServer/Viewshed');
 
-
-
+            
             mapMain.on("load", function () {
                 // Step: Set the spatial reference for output geometries
-    
-
-
+                // console.log('mapMain', mapMain);
+                gp.outSpatialReference = mapMain.spatialReference;
             });
 
             // Collect the input observation point
@@ -50,7 +55,7 @@ require([
             tbDraw.activate(Draw.POINT);
 
             function calculateViewshed(evt) {
-
+                // console.log('calculateViewshed', evt);
                 // clear the graphics layer
                 mapMain.graphics.clear();
 
@@ -65,38 +70,50 @@ require([
                 mapMain.graphics.add(graphicViewpoint);
 
                 // Step: Prepare the first input parameter
-
-
+                // Input_Observation_Point: featureSet
+                var inputPoint = new FeatureSet();
+                // console.log('inputPoint', inputPoint);
+                inputPoint.features.push(graphicViewpoint);
+                // console.log('inputPoint2', inputPoint);
 
                 // Step: Prepare the second input parameter
-
-
+                // Viewshed_Distance distance units
+                var vwDist = new LinearUnit();
+                vwDist.distance = 5;
+                vwDist.units = 'esriMiles';
 
                 // Step: Build the input parameters into a JSON-formatted object
-
-
+                var params = {
+                    'Input_Observation_Point': inputPoint,
+                    'Viewshed_Distance': vwDist
+                };
+                console.log('params', params);
 
                 // Step: Wire and execute the Geoprocessor
-
+                gp.execute(params);
+                gp.on("execute-complete", displayViewshed);
 
 
             }
 
             function displayViewshed(results, messages) {
-
+                console.log('displayViewshed', results)
                 // polygon symbol for drawing results
                 var sfsResultPolygon = new SimpleFillSymbol();
                 sfsResultPolygon.setOutline(new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([255, 255, 0, 0.5]), 1));
                 sfsResultPolygon.setColor(new Color([255, 127, 0, 0.5]));
 
                 // Step: Extract the array of features from the results
-
+                var arrayFeatures = results.results[0].value.features;
 
 
                 // loop through results
                 array.forEach(arrayFeatures, function (feature) {
                     // Step: Symbolize and add each graphic to the map's graphics layer
-    
+                    console.log('feature', feature);
+
+                    var viewshedPolygon = new Graphic(feature.geometry, sfsResultPolygon);
+                    mapMain.graphics.add(viewshedPolygon);
 
 
                 });
